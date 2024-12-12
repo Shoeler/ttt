ae6db60a7b13ae87b490d7269ea6e74819223465
Schuyler Bishop <schuyler.bishop@gmail.com>
2024-12-12 16:43:21 -0600

    Added tests for board and computer

---
+++ board/board_test.go
@@ -0,0 +1,135 @@
+package board
+
+import (
+	"context"
+	"testing"
+
+	"go.opentelemetry.io/otel/trace"
+	"go.opentelemetry.io/otel/trace/noop"
+)
+
+// Create a mock tracer for testing
+func newMockTracer() trace.Tracer {
+	tp := noop.NewTracerProvider() // Initializes a new tracer provider
+	return tp.Tracer("mock-tracer")
+}
+
+func TestCheckWin(t *testing.T) {
+	tracer := newMockTracer()
+	ctx := context.Background()
+
+	tests := []struct {
+		name     string
+		board    [3][3]int
+		expected int
+	}{
+		{
+			name: "Row win for X",
+			board: [3][3]int{
+				{1, 1, 1},
+				{0, 2, 0},
+				{2, 0, 0},
+			},
+			expected: 1,
+		},
+		{
+			name: "Column win for O",
+			board: [3][3]int{
+				{1, 2, 1},
+				{0, 2, 0},
+				{1, 2, 0},
+			},
+			expected: 2,
+		},
+		{
+			name: "Diagonal win for X",
+			board: [3][3]int{
+				{1, 2, 0},
+				{2, 1, 0},
+				{0, 2, 1},
+			},
+			expected: 1,
+		},
+		{
+			name: "Diagonal win for player 0",
+			board: [3][3]int{
+				{2, 1, 0},
+				{1, 2, 0},
+				{0, 1, 2},
+			},
+			expected: 2,
+		},
+		{
+			name: "Draw",
+			board: [3][3]int{
+				{1, 2, 1},
+				{2, 1, 2},
+				{2, 1, 2},
+			},
+			expected: 0,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			result := CheckWin(ctx, tracer, test.board)
+			if result != test.expected {
+				t.Errorf("expected %d, got %d", test.expected, result)
+			}
+		})
+	}
+}
+
+func TestCheckDraw(t *testing.T) {
+	tracer := newMockTracer()
+	ctx := context.Background()
+
+	tests := []struct {
+		name     string
+		board    [3][3]int
+		expected bool
+	}{
+		{
+			name: "Draw",
+			board: [3][3]int{
+				{1, 2, 1},
+				{2, 1, 2},
+				{2, 1, 2},
+			},
+			expected: true,
+		},
+		{
+			name: "Not a draw - empty spaces",
+			board: [3][3]int{
+				{1, 2, 1},
+				{2, 1, 0},
+				{2, 1, 2},
+			},
+			expected: false,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			result := CheckDraw(ctx, tracer, test.board)
+			if result != test.expected {
+				t.Errorf("expected %v, got %v", test.expected, result)
+			}
+		})
+	}
+}
+
+func TestPrintBoard(t *testing.T) {
+	tracer := newMockTracer()
+	ctx := context.Background()
+
+	board := [3][3]int{
+		{1, 2, 1},
+		{2, 0, 2},
+		{1, 2, 1},
+	}
+
+	t.Run("Print board test", func(t *testing.T) {
+		PrintBoard(ctx, tracer, board)
+	})
+}
---
+++ computer/computer_test.go
@@ -0,0 +1,59 @@
+package computer
+
+import (
+	"context"
+	"testing"
+
+	"go.opentelemetry.io/otel/trace"
+	"go.opentelemetry.io/otel/trace/noop"
+)
+
+func newMockTracer() trace.Tracer {
+	tp := noop.NewTracerProvider() // Initializes a new tracer provider
+	return tp.Tracer("mock-tracer")
+}
+
+func TestGetBestMove(t *testing.T) {
+	tracer := newMockTracer()
+	ctx := context.Background()
+
+	tests := []struct {
+		name     string
+		board    [3][3]int
+		computer int
+		human    int
+		expected [2]int
+	}{
+		{
+			name: "Computer(o) should block human (x) win",
+			board: [3][3]int{
+				{2, 0, 1},
+				{0, 1, 0},
+				{0, 0, 0},
+			},
+			human:    1,
+			computer: 2,
+			expected: [2]int{3, 1},
+		},
+		{
+			name: "Computer(x) should block human (o) win",
+			board: [3][3]int{
+				{2, 1, 1},
+				{2, 1, 0},
+				{0, 0, 0},
+			},
+			human:    1,
+			computer: 2,
+			expected: [2]int{3, 1},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			row, col := GetBestMove(ctx, tracer, test.board, test.computer, test.human)
+			if row+1 != test.expected[0] && col+1 != test.expected[1] {
+				t.Errorf("expected %d, got row = %d col = %d", test.expected, row, col)
+			}
+		})
+	}
+}
